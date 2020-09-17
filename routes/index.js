e7bae622c645c6ae8316014e08b9896a2cadcf8c
soheib mohammadi <soheib.mohammadi95@gmail.com>
2020-09-17 19:30:22 +0430

    final edit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
+var io = require("../socketapi").io;
+
+mongoose.connect("mongodb://localhost:27017/chatSocket", (err) => {
+  console.log("mongodb connected", err);
+});
+var Message = mongoose.model("Message", { name: String, message: String });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.sendFile("index.html");
+});
+
+router.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
+
+router.post("/messages", (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) => {
+    if (err) sendStatus(500);
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
